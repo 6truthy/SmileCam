ff63008d5b57cb53d0ccd97a827896ed598152ff
ChaunceyLu <chaunceylu@ChaunceyLus-iMac.local>
2015-03-09 17:23:31 -0700

    Initial Commit

---
+++ Starify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Starify
+//
+//  Created by ChaunceyLu on 3/9/15.
+//  Copyright (c) 2015 ChaunceyLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Starify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Starify
+//
+//  Created by ChaunceyLu on 3/9/15.
+//  Copyright (c) 2015 ChaunceyLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
